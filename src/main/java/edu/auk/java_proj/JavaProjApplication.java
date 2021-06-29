01d1e318f2cad36282b102a3d3d182f15b82e5b5
auk-b57 <amr.kasem.b57@gmail.com>
2021-06-30 01:16:21 +0200

    Fresh Project

---
+++ src/main/java/edu/auk/java_proj/JavaProjApplication.java
@@ -0,0 +1,13 @@
+package edu.auk.java_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/auk/java_proj/JavaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.auk.java_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
