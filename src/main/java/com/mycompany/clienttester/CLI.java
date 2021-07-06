b3888cf82a29521b8ec0a609d1b1b81bea81bd81
auk-b57 <amr.kasem.b57@gmail.com>
2021-07-06 20:59:38 +0200

    added input assertion

--- src/main/java/com/mycompany/clienttester/CLI.java
+++ src/main/java/com/mycompany/clienttester/CLI.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 
 public class CLI {
     private DAO dao = new DAO();
+
     private abstract class Task {
         private String message = "";
 
@@ -24,13 +25,13 @@ public class CLI {
     private Task[] tasks = new Task[] { new Task("Show First 10") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/head/"+Integer.toString(10),-1); // sublisting using api
+            dao.printTable("http://localhost:8080/jobs/head/" + Integer.toString(10), -1); // sublisting using api
             return false;
         }
-    },new Task("Show First 10 with Clean Data") {
+    }, new Task("Show First 10 with Clean Data") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/clean",10); // sublisting using cli manager
+            dao.printTable("http://localhost:8080/jobs/clean", 10); // sublisting using cli manager
             return false;
         }
     }, new Task("Show Structure") {
@@ -48,49 +49,52 @@ public class CLI {
     }, new Task("Show Jobs Per Company") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/jobs_per_company",10);
+            dao.printTable("http://localhost:8080/jobs/jobs_per_company", 10);
             return false;
         }
     }, new Task("Draw Jobs Per Company Pie Chart") {
         @Override
         public boolean action() throws MalformedURLException, IOException {
-            dao.DrawPieChartFromJSON("Top Demanding Companies", 10,true,"company_norm","count", "http://localhost:8080/jobs/jobs_per_company");
+            dao.DrawPieChartFromJSON("Top Demanding Companies", 10, true, "company_norm", "count",
+                    "http://localhost:8080/jobs/jobs_per_company");
             return false;
         }
     }, new Task("Show Jobs Counts") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/jobs_popularity",10);
+            dao.printTable("http://localhost:8080/jobs/jobs_popularity", 10);
             return false;
         }
     }, new Task("Draw Jobs Histo on Bar Chart") {
         @Override
         public boolean action() throws MalformedURLException, IOException {
-            dao.DrawBarChartFromJSON("Top Needed Titles", 20, false,"title_norm","count", "http://localhost:8080/jobs/jobs_popularity");
+            dao.DrawBarChartFromJSON("Top Needed Titles", 20, false, "title_norm", "count",
+                    "http://localhost:8080/jobs/jobs_popularity");
             return false;
         }
     }, new Task("Show Area Counts") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/area_popularity",10);
+            dao.printTable("http://localhost:8080/jobs/area_popularity", 10);
             return false;
         }
     }, new Task("Draw Area Histo on Bar Chart") {
         @Override
         public boolean action() throws MalformedURLException, IOException {
-            dao.DrawBarChartFromJSON("Top Areas", 20, false,"location_norm","count", "http://localhost:8080/jobs/area_popularity");
+            dao.DrawBarChartFromJSON("Top Areas", 20, false, "location_norm", "count",
+                    "http://localhost:8080/jobs/area_popularity");
             return false;
         }
     }, new Task("Show Skills Counts") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/skills",10);
+            dao.printTable("http://localhost:8080/jobs/skills", 10);
             return false;
         }
     }, new Task("Show Jobs with Factorized YearExp") {
         @Override
         public boolean action() throws IOException {
-            dao.printTable("http://localhost:8080/jobs/years_factorized",10);
+            dao.printTable("http://localhost:8080/jobs/years_factorized", 10);
             return false;
         }
     }, new Task("Exit") {
@@ -106,11 +110,19 @@ public class CLI {
             System.out.println("########################################");
             for (int i = 0; i < tasks.length; i++)
                 System.out.println(Integer.toString(i) + ") " + tasks[i].getMessage());
-            
-            System.out.print("Input Command Number: ");
-            int input_value = input.nextInt();
+
+            Integer input_value;
+            do {
+                System.out.print("Input Command Number: ");
+                input_value = input.nextInt();
+                if (input_value > 12 || input_value < 0)
+                    System.out.println("Bad Input...");
+                else
+                    break;
+            } while (true);
+
             System.out.println();
-            if(tasks[input_value].action())
+            if (tasks[input_value].action())
                 break;
         }
         System.out.println("########################################");
